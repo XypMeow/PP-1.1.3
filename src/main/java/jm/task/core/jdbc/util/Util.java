4aba3a8badb418fd0e8649c0d89ff36e50bf175a
XypMeow <anya_varaksina@mail.ru>
2025-07-10 19:20:05 +0300

    ready 1.1.3

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,23 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1f3p1742A!";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            System.err.println("Не удалось подключиться к БД");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
+
